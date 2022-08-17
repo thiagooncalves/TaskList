75b02ef2d9b6b5bd9629e6b8559803de4bbe2200
thiagooncalves <goncalvesthiago1@hotmail.com>
2022-08-17 16:41:42 -0300

    fix: update files table

--- src/components/Tabela/styled.js
+++ src/components/Tabela/styled.js
@@ -2,17 +2,14 @@ import { styled } from "../../stitches.config.js";
 
 export const Tabela = styled("tabela", {
     display: 'flex',
-    width: 700,
+    width: '50vw',
     marginTop: 10,
-    padding: 1,
-    border: 'solid .1rem black',
     backgroundColor: 'black',
     '& th': {
         fontFamily: 'Exo',
-        border: 'solid .1rem black',
+        border: 'solid .1rem white',
         padding: 5,
         width: 218,
-        border: 'solid .15rem white',
         backgroundColor: '#555555',
         color: 'White',
         fontWeight: 'normal',
