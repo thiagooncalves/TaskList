4cdfb78a700bd71490f7a2eda90d89130be896a7
thiagooncalves <goncalvesthiago1@hotmail.com>
2022-08-31 15:39:31 -0300

    fix: correção arquivos

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,27 +9,6 @@ export const Sidebar = () => {
             { links.map( (link) => <button key = { link.toString() }>{ link }</button> )}
           </ul>
         </div>
-        
-        
-        {/* <a href="//www.google.com">
-          <button>{button1}</button>
-        </a>
-        <b />
-        <a href="//www.google.com">
-          <button>{button2}</button>
-        </a>
-        <b />
-        <a href="//www.google.com">
-          <button>{button3}</button>
-        </a>
-        <b />
-        <a href="//www.google.com">
-          <button>{button4}</button>
-        </a>
-        <b />
-        <a href="//www.google.com">
-          <button>{button5}</button>
-        </a> */}
       </S.Sidebar>
   )
 }
